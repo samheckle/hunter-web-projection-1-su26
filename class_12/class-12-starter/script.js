c6b8288c010f9939bbd6976e3468a70d6949422e
samheckle <samheckle@yahoo.com>
2026-07-08 12:08:34 -0400

    added starter

---
+++ class_12/class-12-starter/script.js
@@ -0,0 +1,13 @@
+// What is the first line of code we write in a .js file?
+// Write it below this line.
+
+// Inside of the function, retrieve the id of the div you created in index.html and assign it to a variable.
+
+// Q: What is another way to write the first line of code? 
+// A:
+
+// Q: What does the document keyword represent?
+// A: 
+
+// Q: What is the property we access to change the style of a selected element in js? How would we change the background color of the div we selected?
+// A:
